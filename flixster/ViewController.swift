c5ef9b91366fb42a415fd4cb53e07af83683f9e7
carson <carson@carsons-iMac.local>
2019-02-06 10:22:12 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by carson on 2019-02-06.
+//  Copyright © 2019 CSUMB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
